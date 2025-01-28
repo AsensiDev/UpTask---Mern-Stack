e35d9933fe7debceafd89c85ec0cd392166dfba9
AsensiDev <asensicanopablo3@gmail.com>
2025-01-28 14:50:58 +0100

    auth with token

--- uptask_backend/src/@controllers/AuthController.ts
+++ uptask_backend/src/@controllers/AuthController.ts
@@ -1,6 +1,9 @@
 import type { Request, Response } from 'express'
 import { hashPassword } from '../utils/auth'
 import User from '../@models/User'
+import Token from '../@models/Token'
+import { generateToken } from '../utils/token'
+import { AuthEmail } from '../emails/AuthEmail'
 
 export class AuthController {
 
@@ -22,8 +25,47 @@ export class AuthController {
             // hash password
             user.password = await hashPassword(password)
 
-            await user.save()
+            // generate token
+            const token = new Token()
+            token.token = generateToken()
+            token.user = user.id
+
+            // send email
+            AuthEmail.sendConfirmationEmail({
+                email: user.email,
+                name: user.name,
+                token: token.token
+            })
+
+            await Promise.allSettled([user.save(), token.save()])
             res.send('Cuenta creada, revisa tu email para confirmarla')
+        } catch (error) {
+            res.status(500).json({error: 'Hubo un error'})
+        }
+    }
+
+    static confirmAccount = async (req : Request, res : Response) => {
+        try {
+            // check if token exists
+            const { token } = req.body
+            const tokenExists = await Token.findOne({token})
+            
+            if(!tokenExists) {
+                const error = new Error('El token no es válido')
+                res.status(401).json({error: error.message})
+            }
+
+            // activate user
+            const user = await User.findById(tokenExists.user)
+            user.confirmed = true
+
+            await Promise.allSettled([
+                user.save(), 
+                tokenExists.deleteOne()
+            ])
+
+            res.send('Cuenta confirmada correctamente')
+
         } catch (error) {
             res.status(500).json({error: 'Hubo un error'})
         }
---
+++ uptask_backend/src/emails/AuthEmail.ts
@@ -0,0 +1,28 @@
+import { transporter } from "../config/nodemailer"
+
+interface IEmail {
+    email: string
+    name: string
+    token: string
+}
+
+export class AuthEmail {
+    static sendConfirmationEmail = async ( user : IEmail ) => {
+        const info = await transporter.sendMail({
+            from: 'UpTask <admin@uptask.com>',
+            to: user.email,
+            subject: 'Confirma tu cuenta',
+            text: 'Confirma tu cuenta',
+            html: `<p>Hola: ${user.name}, has creado tu cuenta en Uptask, ya casi está todo listo, solo debes confirmar tu cuenta</p>
+            <p>Para confirmar tu cuenta, haz click en el siguiente enlace:</p>
+            <a href="">Confirmar cuenta</a>
+            <p>E ingresa el código: <b>${user.token}</b></p>
+            <p>Este token expira en 10 minutos</p>            
+            
+            
+            `
+        })
+
+        console.log('mensaje enviado', info.messageId)
+    }
+}
